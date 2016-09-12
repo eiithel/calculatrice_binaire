38b3d62985dad1a2683c6a1f7ff8af06bc6468c0
eiithel <ethel.marquer@gmail.com>
2016-09-12 05:37:15 -0400

    nettoyage sub.asm

--- sub.asm
+++ sub.asm
@@ -47,24 +47,4 @@ _SubEntierSigne32Bits
 
 	.endasmfunc
 
-_proc_3D
-	.asmfunc
-	MVK 0,A0 ; counter pour une boucle pseudo while NOTE INDEX APARTIR DE 1 ET NON 0
-	SUB A4,1,A4
-_etiquette ;label pour retourner
-	LDW *B4++,A7 ; Mettre la valeur de B4 dans A7, et apres incrementer B4 NOTE: LDW mem externe -> mem interne
-	NOP 4 ; Delay
-	MPY 2,A7,A7
-	NOP 1
-	STW A7,*A6++ ; Mettre la valeur de B4 (A travers de A7) dans A6 NOTE: STW mem intern -> mem externe
-	CMPEQ A0,A4,A1 ; if A0 == A4 -> A1 = 1
-	ADD 1,A0,A0 ; increment A0 (counter)
-	[!A1] B _etiquette;
-	NOP 5
-
-
-	B B3
-	NOP 5
-	.endasmfunc
-
 	.end
